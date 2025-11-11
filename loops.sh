e88a37cc01e49c609f75ceec5d89e3c711129e32
syam-devs <syamasundarraodatti@gmail.com>
2025-11-11 12:28:14 +0530

    shell

--- loops.sh
+++ loops.sh
@@ -10,7 +10,7 @@ N="\e[0m"
 LOG_FOLDER="/var/log/shell-script-logs"
 SCRIPT_NAME=$(echo $@ | cut -d '.' -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
-DATE="TZ='Asia/Kolkata' date"
+DATE=$(TZ='Asia/Kolkata' date)
 
 mkdir -p $LOG_FOLDER
 
@@ -20,31 +20,31 @@ echo " script executing at $DATE " | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
-    echo "$R Error: $N you are not running with root user"
+    echo -e "$R Error: $N you are not running with root user"
     exit 1
 else
-    echo "$G you are running with root user $N"
+    echo -e "$G you are running with root user $N"
 fi
 
 VALIDATE(){
     if [ $1 -eq 0 ]
     then 
-        echo "installing.... $2 is $G success $N "
+        echo -e "installing.... $2 is $G success $N " | tee -a $LOG_FILE
     else 
-        echo "installing.... $2 is $R failure $N "
+        echo -e "installing.... $2 is $R failure $N " | tee -a $LOG_FILE
         exit 1
     fi
 }
 
 for pack in $@
 do
-    dnf list module $pack
+    dnf list module $pack &>>$LOG_FILE
     if [ $? -eq 0 ]
     then
-        echo " $pack aready $Y installed..$N "
+        echo -e " $pack aready $Y installed..$N " | tee -a $LOG_FILE
     else
-        echo " $pack $G installing.. $N"
-        dnf install $pack
+        echo -e " $pack $G installing.. $N" | tee -a $LOG_FILE
+        dnf install $pack &>>$LOG_FILE
         VALIDATE $? $pack
     fi
 done
