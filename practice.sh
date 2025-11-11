b6fab25daceb8ef1de3e705780f05bd6b1a7b899
syam-devs <syamasundarraodatti@gmail.com>
2025-11-11 10:44:00 +0530

    shell

--- practice.sh
+++ practice.sh
@@ -5,8 +5,17 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Error:you are not running with root user"
+    exit 1
 else
     echo "you are running with root user"
 fi
 
+
 dnf install mysql -y
+
+if [ $? -eq 0 ]
+then 
+    echo "installing.... mysql is success"
+else 
+    echo "installing.... mysql is failure"
+fi
