bc9d5805be9fb41f7af24d63bab5fe0fdbbfe81b
syam-devs <syamasundarraodatti@gmail.com>
2025-11-11 11:44:18 +0530

    shell

---
+++ loops.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOG_FOLDER="/var/log/shell-script-logs"
+SCRIPT_NAME=$($@ | cut -d '.' -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOG_FOLDER
+
+echo " script executing at $(date) " &>>$LOG_FILE
+
+PACKAGE=("nginx" "mysql" "python3")
+
+if [ $USERID -ne 0 ]
+then
+    echo "Error:you are not running with root user"
+    exit 1
+else
+    echo "you are running with root user"
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then 
+        echo "installing.... $2 is success"
+    else 
+        echo "installing.... $2 is failure"
+        exit 1
+    fi
+}
+
+for pack in $package
+do
+    dnf list module $pack
+    if [ $? -eq 0 ]
+        echo " $pack aready installed.."
+    else
+        echo " $pack installing.."
+        dnf install $pack
+        VALIDATE $? $pack
+    fi
+done
+
