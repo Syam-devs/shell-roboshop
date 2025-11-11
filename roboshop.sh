65d880ee9d7f4c1d4fd05255f334bb279b5124f1
syam-devs <syamasundarraodatti@gmail.com>
2025-11-11 15:14:20 +0530

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-01bc7ebe005fb1cb2" # replace with your SG ID
+SG_ID="sg-00c18b23c2b3c13c0" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z032558618100M4EJX8X4" # replace with your ZONE ID
+# replace with your ZONE ID
  # replace with your domain
 
 #for instance in ${INSTANCES[@]}
