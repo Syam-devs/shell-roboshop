cbcb8aa4f2be9acd1836093c6102ee0b71a0cf72
syam-devs <syamasundarraodatti@gmail.com>
2025-11-11 15:07:49 +0530

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -5,3 +5,23 @@ SECURITY_GROUP_IDS="sg-00c18b23c2b3c13c0"
 INSTANCE_NAME="Shell-Script-EC2-Demo"
 aws ec2 run-instances "$AMI_ID" "$INSTANCE_TYPE" "$SECURITY_GROUP_IDS" "$INSTANCE_NAME"
 echo "EC2 instance creation completed."
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-01bc7ebe005fb1cb2" # replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z032558618100M4EJX8X4" # replace with your ZONE ID
+ # replace with your domain
+
+#for instance in ${INSTANCES[@]}
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+    echo "$instance IP address: $IP"
