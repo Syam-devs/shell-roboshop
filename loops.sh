b73e2551fb663b822b61ae1328ccb7458585c620
syam-devs <syamasundarraodatti@gmail.com>
2025-11-11 12:15:30 +0530

    shell

--- loops.sh
+++ loops.sh
@@ -2,42 +2,48 @@
 
 USERID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 LOG_FOLDER="/var/log/shell-script-logs"
-SCRIPT_NAME=$($@ | cut -d '.' -f1)
+SCRIPT_NAME=$(echo $@ | cut -d '.' -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+DATE="TZ='Asia/Kolkata' date"
 
 mkdir -p $LOG_FOLDER
 
-echo " script executing at $(date) " &>>$LOG_FILE
+echo " script executing at $DATE " | tee -a $LOG_FILE
 
-PACKAGE=("nginx" "mysql" "python3")
+
 
 if [ $USERID -ne 0 ]
 then
-    echo "Error:you are not running with root user"
+    echo "$R Error: $N you are not running with root user"
     exit 1
 else
-    echo "you are running with root user"
+    echo "$G you are running with root user $N"
 fi
 
 VALIDATE(){
     if [ $1 -eq 0 ]
     then 
-        echo "installing.... $2 is success"
+        echo "installing.... $2 is $G success $N "
     else 
-        echo "installing.... $2 is failure"
+        echo "installing.... $2 is $R failure $N "
         exit 1
     fi
 }
 
-for pack in ${PACKAGE[@]}
+for pack in $@
 do
     dnf list module $pack
     if [ $? -eq 0 ]
     then
-        echo " $pack aready installed.."
+        echo " $pack aready $Y installed..$N "
     else
-        echo " $pack installing.."
+        echo " $pack $G installing.. $N"
         dnf install $pack
         VALIDATE $? $pack
     fi
